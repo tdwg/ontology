3424bb80f0fc3dfad0cc4f87b1aea20e94a6a503
rogerhyam <rogerhyam@ef606228-d37b-11de-ba7e-09ca73f1295e>
2009-12-10 13:48:23 +0000

    removal of tapir directory and contents as they shouldn't be in the ontology

--- ontology/voc/tapir/structure/Test/simple.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE  `TAPIRUpdater`.`simple` (
-  `id` int(11) NOT NULL auto_increment,
-  `verbatimLatitude` varchar(100) default NULL,
-  `verbatimLongitude` varchar(100) default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8
